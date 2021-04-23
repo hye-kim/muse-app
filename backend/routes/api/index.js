6768953c901ab4d74cd9738e667f382645b8a723
hye <davidckim22@gmail.com>
2021-04-23 12:36:48 -0400

    first commit

---
+++ backend/routes/api/index.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
+router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'DemoUser'
+      },
+    })
+  setTokenCookie(res, user);
+  return res.json({ user });
+}));
+
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+router.post('/test', function(req, res) {
+    res.json({ requestBody: req.body });
+  });
+
+module.exports = router;
